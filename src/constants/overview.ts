a12d3381b4687a40388ef2601a8e7cdc93224568
spark <innocentchris@gmail.com>
2025-12-29 22:58:47 +0900

    문구 수정

--- src/constants/overview.ts
+++ src/constants/overview.ts
@@ -8,6 +8,6 @@ export const OVERVIEW_DATA = {
   communityBuilder: {
     title: "AWS Community Builder란?",
     description:
-      "AWS Community Builder 프로그램은 AWS에 대한 열정과 지식을\n공유하는 기술 커뮤니티 리더들을 위한 글로벌 프로그램입니다.\n\n이들은 AWS 제품 팀 및 히어로들과 긴밀하게 소통하며 AWS가 제공하는 풍부한 기술 리소스와 네트워킹 기회를 통해 기술적 전문성을 쌓고 이를 커뮤니티에 전파하는 역할을 수행합니다.",
+      "AWS Community Builder 프로그램은 AWS에 대한 열정과 지식을\n공유하는 기술 커뮤니티 리더들을 위한 글로벌 프로그램입니다.\n\nCommunity Builder는 블로그 포스트, 영상, 발표 등의 AWS 관련 컨텐츠 생산 및 활동을 기준으로 1년에 한번씩 선정됩니다.\n\n선정된 Community Builder들은 AWS 제품 팀 및 Hero들과 긴밀하게 소통하며 AWS가 제공하는 풍부한 기술 리소스와 네트워킹 기회를 통해 기술적 전문성을 쌓고 이를 커뮤니티에 전파하는 역할을 수행합니다.",
   },
 } as const;
